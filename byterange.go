35329f0a0eab89e7c7973623c033b9c91dd05416
David McLeish <david.mcleish@nearmap.com>
2022-08-19 14:08:21 +1000

    bugfix

--- byterange.go
+++ byterange.go
@@ -13,7 +13,7 @@ func RangeKey(key string, start, end int64) string {
 func KeyToRange(rk string) (string, int64, int64, bool) {
 	var key string
 	var start, end int64
-	if n, err := fmt.Sscanf(key, "%s$$%d-%d", &key, &start, &end); err != nil && n == 3 {
+	if n, err := fmt.Sscanf(rk, "%s$$%d-%d", &key, &start, &end); err != nil && n == 3 {
 		key = strings.ReplaceAll(key, "\\$", "$")
 		return key, start, end, true
 	}
