fd3a9599905f2759549cf79c87f050973558873a
David McLeish <david.mcleish@nearmap.com>
2022-08-19 14:26:47 +1000

    use regexp for range key

--- byterange.go
+++ byterange.go
@@ -2,20 +2,27 @@ package groupcache
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
+var re = regexp.MustCompile(`^(.*)##(\d+):(\d+)$`)
+
 func RangeKey(key string, start, end int64) string {
-	key = strings.ReplaceAll(key, "$", "\\$")
-	return fmt.Sprintf("%s$$%d-%d", key, start, end)
+	key = strings.ReplaceAll(key, `#`, `\#`)
+	return fmt.Sprintf("%s##%d:%d", key, start, end)
 }
 
 func KeyToRange(rk string) (string, int64, int64, bool) {
-	var key string
-	var start, end int64
-	if n, err := fmt.Sscanf(rk, "%s$$%d-%d", &key, &start, &end); err != nil && n == 3 {
-		key = strings.ReplaceAll(key, "\\$", "$")
-		return key, start, end, true
+	match := re.FindStringSubmatch(rk)
+	if len(match) == 4 {
+		key := strings.ReplaceAll(match[1], `\#`, `#`)
+		start, err1 := strconv.ParseInt(match[2], 10, 64)
+		end, err2 := strconv.ParseInt(match[3], 10, 64)
+		if err1 == nil && err2 == nil {
+			return key, start, end, true
+		}
 	}
 	return rk, 0, 0, false
 }
